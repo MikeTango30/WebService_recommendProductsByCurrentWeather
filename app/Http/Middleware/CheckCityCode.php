5e5aabb6daba209c6c5f63c30d05c58c3315e75c
Mantas Tarutis <mantas.tarutis@gmail.com>
2020-02-03 20:49:40 +0200

    make given city param lowercase

--- app/Http/Middleware/CheckCityCode.php
+++ app/Http/Middleware/CheckCityCode.php
@@ -17,6 +17,7 @@ class CheckCityCode
     public function handle($request, Closure $next)
     {
         $city = last(explode("/", $request->getUri()));
+        $city = strtolower($city);
         $cities = json_decode(file_get_contents(base_path('database/repositories/place_codes.json')));
         if (!in_array($city, $cities)) {
             abort(404, "Wrong city name");
