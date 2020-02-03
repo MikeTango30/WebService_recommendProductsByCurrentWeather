c8bbd0d6ae73ac376336b3dea76a8ace4b3b56be
Mantas Tarutis <mantas.tarutis@gmail.com>
2020-02-03 15:13:47 +0200

    index page

---
+++ resources/views/info.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Recommended Products Service</title>
+</head>
+<body>
+<main>
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="usage">
+                        <h4>Api usage</h4>
+                        <ul>
+                            <li>https://recommended-products.herokuapp.com/api/products/recommended/:city</li>
+                            <li>where :city enter any LT city without LT characters and with dashes instead of spaces</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+</body>
+</html>
