57727b3d08bea270c7c1d485abd53bee9b9b6c4d
nomaxas <nomaxas@gmail.com>
2020-02-03 17:11:50 +0200

    '/'

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -14,26 +14,32 @@
 <main>
     <div class="container-fluid">
         <div class="container">
-            <div class="row">
-                <div class="col">
-                    <div class="card mt-6">
-                        <div class="card-header">Api usage</div>
-                        <div class="card-body">
-                            <h5>Go to:</h5>
-                            <ul>
-                                <li>https://recommended-products.herokuapp.com/api/products/recommended/<span>:city</span></li>
-                                <li>where <span>:city</span> enter any LT city without LT characters and with dashes instead of spaces
-                                </li>
-                            </ul>
-                        </div>
+            <div class="row justify-content-center mt-6">
+                <div class="card">
+                    <div class="card-header">Api usage</div>
+                    <div class="card-body">
+                        <h5>Go to:</h5>
+                        <ol>
+                            <li>https://recommended-products.herokuapp.com/api/products/recommended/<span>:city</span>
+                            </li>
+                            <li>where <span>:city</span> enter any LT city without LT characters and with dashes instead
+                                of spaces
+                            </li>
+                        </ol>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </main>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
 </body>
 </html>
